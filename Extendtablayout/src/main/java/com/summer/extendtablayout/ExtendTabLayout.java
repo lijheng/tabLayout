eec069cec2d1b053d3a1c3545a2932495507f2cc
summer <1107285162@qq.com>
2019-07-30 17:33:01 +0800

    新增选中tab text大小属性

--- Extendtablayout/src/main/java/com/summer/extendtablayout/ExtendTabLayout.java
+++ Extendtablayout/src/main/java/com/summer/extendtablayout/ExtendTabLayout.java
@@ -26,7 +26,7 @@ import com.google.android.material.tabs.TabLayout;
  * 通过customView的方式改变tabLayout的indicator，让indicator的宽度等于文字的宽度
  * 且 可以通过属性设置indicator距离tabLayout的左侧右侧距离
  */
-public class ExtendTabLayout extends TabLayout {
+public class ExtendTabLayout extends TabLayout implements TabLayout.BaseOnTabSelectedListener {
 
     private final int indicatorHeight;
     @ColorInt
@@ -38,13 +38,13 @@ public class ExtendTabLayout extends TabLayout {
     private @DrawableRes
     int indicatorDrawable;
 
-    private float tabTextSize;
+    private float tabTextSize, tabSelectTextSize, defaultTextSize;
 
     private final int indicatorMarginStart;
 
     private final int indicatorMarginEnd;
 
-    private final int indicatorOverStart,indicatorOverEnd;
+    private final int indicatorOverStart, indicatorOverEnd;
 
     public ExtendTabLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, com.google.android.material.R.attr.tabStyle);
@@ -60,9 +60,12 @@ public class ExtendTabLayout extends TabLayout {
         tabTextSize = (float) array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabTextSize, 0);
         indicatorDrawable = array.getResourceId(R.styleable.ExtendTabLayout_extendTabIndicatorDrawable, 0);
         indicatorWidth = array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabIndicatorWidth, 0);
-        indicatorOverStart = array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabIndicatorOverStart,0);
-        indicatorOverEnd = array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabIndicatorOverEnd,0);
+        indicatorOverStart = array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabIndicatorOverStart, 0);
+        indicatorOverEnd = array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabIndicatorOverEnd, 0);
+        tabSelectTextSize = (float) array.getDimensionPixelSize(R.styleable.ExtendTabLayout_extendTabSelectTextSize, 0);
         array.recycle();
+
+        addOnTabSelectedListener(this);
     }
 
 
@@ -82,8 +85,10 @@ public class ExtendTabLayout extends TabLayout {
         textView.setTextColor(getTabTextColors());
         if (tabTextSize != 0f) {
             textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, tabTextSize);
+        } else {
+            defaultTextSize = textView.getTextSize();
         }
-        textView.setPadding(indicatorOverStart,0,indicatorOverEnd,0);
+        textView.setPadding(indicatorOverStart, 0, indicatorOverEnd, 0);
         View indicator = view.findViewById(R.id.layout_extend_tab_indicator);
         ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) indicator.getLayoutParams();
         params.height = indicatorHeight;
@@ -113,5 +118,37 @@ public class ExtendTabLayout extends TabLayout {
         stateListDrawable.addState(new int[]{android.R.attr.state_selected}, drawable);
         stateListDrawable.addState(new int[]{}, new ColorDrawable());
         return stateListDrawable;
+    }
+
+    @Override
+    public void onTabSelected(Tab tab) {
+        if (((int) tabSelectTextSize) == 0) return;
+        changeTabTextSize(tab, tabSelectTextSize);
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(Tab tab) {
+        if (((int) tabSelectTextSize) == 0) return;
+        changeTabTextSize(tab, ((int) tabTextSize) == 0 ? defaultTextSize : tabTextSize);
+    }
+
+    /**
+     * 改变tab的字体大小
+     *
+     * @param tab
+     * @param textSize
+     */
+    private void changeTabTextSize(Tab tab, float textSize) {
+        View view = tab.getCustomView();
+        if (view == null) return;
+        TextView tv = view.findViewById(R.id.layout_extend_tab_tv_content);
+        if (tv != null) {
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        }
     }
 }
